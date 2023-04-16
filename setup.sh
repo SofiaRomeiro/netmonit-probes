f2b8851ca1b95c7a19034db27d2447db7929d04d
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2023-04-16 22:28:24 +0100

    automatize name attribution on probes

--- setup.sh
+++ setup.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#setup pi variables
+echo "Starting bash script..."
+
+name="NAME = '"$1"'"
+location="PI_LOCATION = '"$2"'"
+model="PI_MODEL = '"$3"'"
+
+echo "" >> "./pi/env.py"
+echo "$name" >> "./pi/env.py"
+echo "$location" >> "./pi/env.py"
+echo "$model" >> "./pi/env.py"
+
+sleep 1
+
 sudo apt-get install -y iperf3
 sudo docker-compose build
 sudo docker-compose up
