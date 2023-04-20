a573cc826b99d3a251e2fffc3c2045596ce0578e
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2023-04-20 22:20:05 +0100

    fix values on db

--- database/dumps/init_pi.sql
+++ database/dumps/init_pi.sql
@@ -12,8 +12,8 @@ CREATE TABLE events (
     avg BIGINT,
     packets_sent INTEGER,
     packets_received INTEGER,
-    packet_loss DECIMAL(4,1),
-    jitter DECIMAL(4,1),
+    packet_loss DECIMAL(3,1),
+    jitter DECIMAL(6,3),
     interface VARCHAR(20),
     PRIMARY KEY (creation_date)
 );
